b6882cc4a9d659d03f031938f3e3adb9293a40df
Dayana Hoyos Riascos <da.hoyos@duocuc.cl>
2025-05-24 15:53:45 -0400

    bd prueba

--- ferremas/ferremas_back/models.py
+++ ferremas/ferremas_back/models.py
@@ -1,4 +1,26 @@
 from django.db import models
 
 
+class Categoria(models.Model):
+    id= models.CharField(max_length=10, primary_key=True)
+    nombre= models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.id} {self.nombre}"
+
+class Producto(models.Model):
+    id = models.CharField(max_length=20, primary_key=True) 
+    marca = models.CharField(max_length=30)
+    cod_ext = models.CharField(max_length=15)
+    nombre = models.CharField(max_length=100)
+    precio = models.IntegerField
+    fec_modif = models.DateField
+    categoria= models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    stock= models.IntegerField
+
+    def __str__(self):
+       return f"{self.id} {self.marca} {self.nombre} {self.precio}"
+
+
+
+    
