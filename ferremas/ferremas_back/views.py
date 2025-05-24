54ca381577b8ea9c207d11061f8208179956aef4
YhonVivas <yohnvivas16@gmail.com>
2025-05-24 17:11:18 -0400

    Se agrega api getStock

--- ferremas/ferremas_back/views.py
+++ ferremas/ferremas_back/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Producto
+from .serializers import ProductoSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def getStock(request):
+    productos = Producto.objects.all()
+    serializer = ProductoSerializer(productos, many=True)
+    print(serializer)
+    return Response(serializer.data)
+
