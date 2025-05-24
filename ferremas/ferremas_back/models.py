220d9319a50d1d3a6783756fba2e223c60ac57a9
YhonVivas <yohnvivas16@gmail.com>
2025-05-24 16:01:46 -0400

    Creado las tablas

--- ferremas/ferremas_back/models.py
+++ ferremas/ferremas_back/models.py
@@ -3,20 +3,20 @@ from django.db import models
 
 class Categoria(models.Model):
     id= models.CharField(max_length=10, primary_key=True)
-    nombre= models.CharField(max_length=100)
+    nombre= models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f"{self.id} {self.nombre}"
 
 class Producto(models.Model):
     id = models.CharField(max_length=20, primary_key=True) 
-    marca = models.CharField(max_length=30)
-    cod_ext = models.CharField(max_length=15)
-    nombre = models.CharField(max_length=100)
-    precio = models.IntegerField
-    fec_modif = models.DateField
+    marca = models.CharField(max_length=30, null=True)
+    cod_ext = models.CharField(max_length=15, null=True)
+    nombre = models.CharField(max_length=100, null=True)
+    precio = models.IntegerField(null=True)
+    fec_modif = models.DateField(null=True)
     categoria= models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    stock= models.IntegerField
+    stock= models.IntegerField(null=True)
 
     def __str__(self):
        return f"{self.id} {self.marca} {self.nombre} {self.precio}"
